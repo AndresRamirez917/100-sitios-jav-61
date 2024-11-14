4b2f7dd2d926e068e98d0946a65bfaa29198575a
AndresRamirez917 <andresmintic2021@gmail.com>
2024-11-14 11:36:33 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ async function getData() {
     console.log(productsArr);
     const productsArrSliced = productsArr.slice(0,5);
     products.forEach(element => {
-        const randData= (min, max) => Math.floor(Math.random() * (min - max + 1) + max);
+        const randData = (min, max) => Math.floor(Math.random() * (min - max + 1) + max);
         const randInt = randData(1, productsArr.length);
         const randIndex = randInt;
         for(i = 0; i < productsArrSliced.length; i++){
@@ -30,3 +30,46 @@ async function getData() {
     });
 }
 getData()
+
+const btn_validar = document.getElementById('btn-validar').onclick = (e) => {
+    e.preventDefault();
+    const nombre = document.getElementById('nombre');
+    const email = document.getElementById('email');
+    const fecha = document.getElementById('fecha');
+    const hora = document.getElementById('hora');
+    const mensaje = document.getElementById('mensaje');
+    const arr = [nombre, email, fecha, hora, mensaje];
+    const messageArr = ["Nombre", "Email", "Fecha", "Hora", "Mensaje"];
+    for(i = 0; i < arr.length; i++){
+        if(arr[i].value == ""){
+            swal({
+                title: `El campo ${messageArr[i]} no puede estar vacío`,
+                icon: "error",
+                 })
+                 return false
+        }
+    }
+    if(!emailValido(email)){
+        swal({
+            title: `El campo ${messageArr[1]} no tiene el formato correcto`,
+            icon: "error",
+             })
+             return false;
+    }
+    swal({
+        title: `Datos enviados satisfactoriamente`,
+        icon: "success",
+         })
+         nombre.value = "";
+         email.value = "";
+         fecha.value = "";
+         hora.value = "";
+         mensaje.value = "";
+         return true;
+
+}
+
+const emailValido = (email) => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value)
+
+}
